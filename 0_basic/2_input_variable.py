0d361a439fabb0d47c27d11531b72cabd3e3a041
ganesa prabu <41106620+ganesaprabu@users.noreply.github.com>
2021-11-20 11:29:28 +0000

    Added comments

--- 0_basic/2_input_variable.py
+++ 0_basic/2_input_variable.py
@@ -1,9 +1,11 @@
+#### String value
 print('What is your name: ')
 name = str(input())
 
 print('What is your place: ')
 place = str(input())
 
+#### Integer value
 print('What is your age: ')
 age = int(input())
 
