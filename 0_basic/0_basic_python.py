5ea87411779e3ba550c0ad986ea80bb0f6224f46
ganesa prabu <41106620+ganesaprabu@users.noreply.github.com>
2021-11-20 10:40:49 +0000

    Different ways of printing

--- 0_basic/0_basic_python.py
+++ 0_basic/0_basic_python.py
@@ -1,3 +1,10 @@
 print("This is Success")
 print("This is Printing in Console")
 print("Hello world !!!")
+
+# We can print integer and decimals
+print(10)
+print(10.89)
+
+# We can print multiple in the same print
+print('String #1', 'String #2', 'String #3', 100)
