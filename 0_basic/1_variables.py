eb32bdb056bf6315ff3be047153206aa64b1af0f
ganesa prabu <41106620+ganesaprabu@users.noreply.github.com>
2021-11-20 10:55:17 +0000

    Variable assignment

---
+++ 0_basic/1_variables.py
@@ -0,0 +1,16 @@
+# Assigning the variables
+
+# Assigning the integer
+x = 5
+y = 11
+a = x + y
+print(x)
+print(y)
+print(a)
+
+# Assigning the String
+name = 'Madurai'
+print(name)
+
+# Printing multiple values
+print(x, y, a, name)
