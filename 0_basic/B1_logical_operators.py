6cb5f4c7916677a126cb7ec3bd1e3ef754e731ec
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2021-11-21 09:40:32 +0000

    File name changed based on Order

---
+++ 0_basic/B1_logical_operators.py
@@ -0,0 +1,20 @@
+# Logical operator AND
+print('# Logical operator AND [TRUE if all are true]')
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print()
+
+print('# Logical operator OR [TRUE if any one is true]')
+# Logical operator OR
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print()
+
+print('# Logical operator NOT [inverse of]')
+# Logical operator OR
+print(not True)
+print(not (True))
