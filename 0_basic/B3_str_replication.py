2218d0080f4f3521123b6310a437d2be42e3eeb9
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2021-11-24 09:44:40 +0000

    Adding comments

--- 0_basic/B3_str_replication.py
+++ 0_basic/B3_str_replication.py
@@ -1,7 +1,8 @@
 # String replication
 name = 'Game'
-print(name * 5) 
-print(name[0] * 5)
+print(name * 5) # GameGameGameGameGame
+# Replicating specific Char
+print(name[0] * 5) #GGGGG
 
 
 # String comparison
