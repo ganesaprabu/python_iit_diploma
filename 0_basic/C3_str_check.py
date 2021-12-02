6c8aa5ffc0723134c0dba727a7440039700ff3e5
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2021-12-02 17:26:16 +0000

    Conditional check

--- 0_basic/C3_str_check.py
+++ 0_basic/C3_str_check.py
@@ -1,3 +1,8 @@
 # Prints true or false based on the condition
 txt = "The best things in life are free!"
 print("free" in txt)
+
+# Conditional check
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
