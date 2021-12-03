5e94c321f77e671336d74c8004e7955ed9308bb7
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2021-12-03 05:09:18 +0000

    Comments

--- 0_basic/C3_str_check.py
+++ 0_basic/C3_str_check.py
@@ -5,4 +5,4 @@ print("free" in txt)
 # Conditional check
 txt = "The best things in life are free!"
 if "free" in txt:
-  print("Yes, 'free' is present.") #Needs an indendation otherwise error
+  print("Yes, 'free' is present.") #Needs an indendation otherwise error. No curly braces as like Java
