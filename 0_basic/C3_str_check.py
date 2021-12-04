fe6e1e362c653d46720a23cd3492726e5a171547
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2021-12-04 01:19:26 +0000

    With If condition

--- 0_basic/C3_str_check.py
+++ 0_basic/C3_str_check.py
@@ -15,3 +15,8 @@ print()
 txt = "The best things in life are free!"
 print("expensive" not in txt)
 
+#Check if NOT (Example with If condition)
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
