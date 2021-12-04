5d8b505e8cbe089281e1831ba0b72f6d780bf53a
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2021-12-04 01:15:09 +0000

    Comment

--- 0_basic/C3_str_check.py
+++ 0_basic/C3_str_check.py
@@ -9,8 +9,9 @@ txt = "The best things in life are free!"
 if "free" in txt:
   print("Yes, 'free' is present.") #Needs an indendation otherwise error. No curly braces as like Java
 
-
 print()
+
+#Check if NOT
 txt = "The best things in life are free!"
 print("expensive" not in txt)
 
