cf89bc09ba96a32c478828a060bcb25ca5b010aa
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-01-01 07:36:25 +0000

    Comments for Arbitrary Arguments

--- 0_basic/C5_function.py
+++ 0_basic/C5_function.py
@@ -14,7 +14,7 @@ def my_function_welcome_msg(name, salutation):
   print("Hi ", salutation , name, ", Welcome to Python")
 my_function_welcome_msg("John", "Mr.");
 
-
+# Arbitrary Arguments, *args
 def my_function(*team):
   print("One of the team member is  " + team[2])
 my_function("Jill", "Jam", "Sweety")
