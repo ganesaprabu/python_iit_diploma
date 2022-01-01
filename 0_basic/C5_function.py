2cbbe4774a441cdfc4c00d0771852dfd06700713
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-01-01 07:33:23 +0000

    More params in function

--- 0_basic/C5_function.py
+++ 0_basic/C5_function.py
@@ -7,6 +7,9 @@ my_function();
 # Parameterized
 def my_function_year(year):
   print("Happy new year", year)
-
 my_function_year(2022);
 
+
+def my_function_welcome_msg(name, salutation):
+  print("Hi ", salutation , name, ", Welcome to Python")
+my_function_welcome_msg("John", "Mr.");
