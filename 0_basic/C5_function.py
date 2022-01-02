8d7cb0b3275a114c6ba5c8fe5b96a4b17e42248f
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-01-02 06:45:00 +0000

    Default function value

--- 0_basic/C5_function.py
+++ 0_basic/C5_function.py
@@ -18,3 +18,8 @@ my_function_welcome_msg("John", "Mr.");
 def my_function(*team):
   print("One of the team member is  " + team[2])
 my_function("Jill", "Jam", "Sweety")
+
+def my_default_fun(city = "Chennai"):
+  print("I am from " + city)
+my_default_fun("Madurai")
+my_default_fun()
