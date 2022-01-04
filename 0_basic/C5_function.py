d4afcb16483a44dec2b95923525b3270db40a59c
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-01-04 00:37:31 +0000

    Comments

--- 0_basic/C5_function.py
+++ 0_basic/C5_function.py
@@ -15,9 +15,10 @@ def my_function_welcome_msg(name, salutation):
 my_function_welcome_msg("John", "Mr.");
 
 # Arbitrary Arguments, *args
-def my_function(*team):
-  print("One of the team member is  " + team[2])
-my_function("Jill", "Jam", "Sweety")
+def my_function_args(*team):
+  print("One of the team member is  " + team[1])
+my_function_args("Jill", "Jam", "Sweety")
+my_function_args()
 
 # Default value function
 # If caller doesn't provide the value, default will work
