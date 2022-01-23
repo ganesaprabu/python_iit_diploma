854c6592c8a145b48214615042d21dbd73569dae
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-01-23 05:03:55 +0000

    CHanges

--- 0_basic/A5_data_conversion.py
+++ 0_basic/A5_data_conversion.py
@@ -1,7 +1,10 @@
-# INT conversion
-print('# INT conversion')
+# FLOAT conversion
+print('# FLOAT conversion')
 x = int(4.5)
 print(x)
+
+# INT conversion
+print('# INT conversion')
 y = int('4')
 print(y)
 
