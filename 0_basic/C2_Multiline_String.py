7f05c32117ca18c71a79d546bb02a169a2b7339b
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-02-03 05:01:39 +0000

    Comments

--- 0_basic/C2_Multiline_String.py
+++ 0_basic/C2_Multiline_String.py
@@ -14,6 +14,6 @@ print()
 print(b)
 
 print()
-# In will print all the objects which is in the parameters passed to print method
+# In will print all the objects which is in the parameters passed to print method as arguments
 print(a, b)
 
