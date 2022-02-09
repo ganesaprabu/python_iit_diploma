2d98344cf3311a8c3f13fc1e6d3417ab54c066d9
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-02-09 02:29:45 +0000

    Changes

--- 0_basic/A2_input_variable.py
+++ 0_basic/A2_input_variable.py
@@ -9,6 +9,6 @@ place = str(input())
 print('What is your age: ')
 age = int(input())
 
-# Multiple values as the arguments
+# Multiple values as the arguments will be printed with space
 print()
 print('Hi', name, '. Good to know that you are from ', place, '. Good to know that you are', age, 'old')
