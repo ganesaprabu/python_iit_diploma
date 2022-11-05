b1b380d70c3b39b9009168fcfbe6768532cb8634
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-11-05 10:58:38 +0530

    Spell check

--- 0_basic/B5_Str_Comparison.py
+++ 0_basic/B5_Str_Comparison.py
@@ -1,8 +1,7 @@
-print('String Compariosn')
+print('String Comparison')
 # String comparison
 # will check the charecters one by one
 # Below will result in True
 print('a' < 'z')
 # In Below 'ABC' is matched, in the left 'D' is extra and hence it is greater and results in True
 print('ABCD' > 'ABC')
-
