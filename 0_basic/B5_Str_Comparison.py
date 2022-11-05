c990cafb05cdc6eae8153aad853b4b0a823e06e9
Ganesaprabu Navamanirajan <41106620+ganesaprabu@users.noreply.github.com>
2022-11-05 10:57:03 +0530

    String changes

--- 0_basic/B5_Str_Comparison.py
+++ 0_basic/B5_Str_Comparison.py
@@ -1,7 +1,8 @@
-print('Compariosn')
+print('String Compariosn')
 # String comparison
 # will check the charecters one by one
 # Below will result in True
 print('a' < 'z')
 # In Below 'ABC' is matched, in the left 'D' is extra and hence it is greater and results in True
 print('ABCD' > 'ABC')
+
